9597c284b0d2f153e1ad2900b31b8b944208693b
Rupesh <shawrupesh020@gmail.com>
2025-05-02 07:43:35 +0530

    chips input

--- src/App.js
+++ src/App.js
@@ -1,144 +1,72 @@
 import { useState } from "react";
-import data from "../src/data.json";
 import "../src/App.css";
 
-const DisplayList = ({ filedata, setFileData }) => {
-  const [expand, setExpanded] = useState({});
-  const addNode = (id) => {
-    const folderName = prompt("Enter Folder Name");
-    function upDateTree(list) {
-      return list.map((node) => {
-        if (node.id === id) {
-          return {
-            ...node,
-            children: [
-              ...node.children,
-              {
-                name: folderName,
-                type: "folder",
-                id: Math.ceil(Math.random * 1000),
-                children: [],
-              },
-            ],
-          };
-        }
-        if (node.children) {
-          return {
-            ...node,
-            children: upDateTree(node.children),
-          };
-        }
-        return node;
-      });
+function App() {
+  const [chipsContainer, setchipsContainer] = useState([]);
+  const [input, setInput] = useState("");
+
+  function handelTags(e) {
+    if (e.key === "Enter") {
+      if (!(input === 0) && !input.includes(" ")) {
+        setchipsContainer((prev) => [
+          ...prev,
+          { name: input, id: Math.ceil(Math.random() * 1000) },
+        ]);
+      }
+      setInput("");
+    }
+  }
+   function handelOnChnage(e){
+    setInput(e.target.value);
+
+
+
+  }
+
+  function handelDelete(id) {
+    function filterData() {
+      return chipsContainer.filter((item) => item.id !== id);
     }
 
-    setFileData((prev) => upDateTree(prev));
-  };
-
-  const deleteNode = (id) => {
-    function deleteTree(list) {
-      console.log("inside delete tree", list);
-      return list
-        .filter((node) => node.id !== id)
-        .map((node) => {
-          if (node.children) {
-            return {
-              ...node,
-              children: deleteTree(node.children),
-            };
-          }
-          return node;
-        });
-    }
-    setFileData((prev) => deleteTree(prev));
-  };
-
-  const handleExpand = (item) => {
-    setExpanded((prev) => ({ ...prev, [item.name]: !prev[item.name] }));
-  };
-  console.log("expanded", expand);
+    setchipsContainer((prev) => filterData());
+  }
+  console.log(chipsContainer);
 
   return (
     <>
-      {filedata.map((item) => {
-        return (
-          <div className="parent">
-            {item.type === "folder" && (
-              <span
-                onClick={() => {
-                  handleExpand(item);
-                }}
-              >
-                {!expand[item.name] && (
-                  <svg
-                    className="svg-down"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 512 512"
-                  >
-                    <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
-                  </svg>
-                )}
-
-                {expand[item.name] && (
-                  <svg
-                    className="svg-down"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 448 512"
-                  >
-                    <path d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z" />
-                  </svg>
-                )}
-              </span>
-            )}
-            <span key={item.id}>{item.name} </span>
-           
-            {item.type === "folder" && (
+      <div className="chip-container">
+        <h1>Chips Input</h1>
+        <input
+          name="chips"
+          type="text"
+          placeholder="add tags"
+          onKeyDown={handelTags}
+          onChange={handelOnChnage}
+          value={input}
+        />
+        <br></br>
+        <div></div>
+        <div className="tags">
+          {chipsContainer.map((tag) => {
+            return (
               <>
-               <span>({item?.children?.length})</span>
-              <span
-                onClick={() => {
-                  addNode(item?.id);
-                }}
-              >
-                +
-              </span>
+                <div key={tag?.id} className="span-tag">
+                  <span>{tag?.name}</span>
+                  <button
+                    onClick={() => {
+                      handelDelete(tag.id);
+                    }}
+                  >
+                    X
+                  </button>
+                </div>
               </>
-              
-            )}
-            <span
-              onClick={() => deleteNode(item.id)}
-              style={{ marginLeft: "10px", cursor: "pointer" }}
-            >
-              Delete
-            </span>
-            {item?.children?.length > 0 && (
-              <div className="children">
-                {expand[item.name] && (
-                  <DisplayList
-                    key={item.id}
-                    filedata={item?.children}
-                    setFileData={setFileData}
-                  />
-                )}
-              </div>
-            )}
-          </div>
-        );
-      })}
-    </>
-  );
-};
-
-export default function App() {
-  const [filedata, setFileData] = useState(data);
-  console.log("fileData", filedata);
-
-  return (
-    <>
-      <div className="parent">
-        <h1>File Explorer</h1>
+            );
+          })}
+        </div>
       </div>
-      <DisplayList filedata={filedata} setFileData={setFileData} />
     </>
   );
 }
+
+export default App;
